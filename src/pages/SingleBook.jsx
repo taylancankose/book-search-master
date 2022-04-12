3057660a8a6e4fc0fd5df174937e1b04903350ad
tecosama <taylancankose@gmail.com>
2022-04-12 15:30:10 +0300

    a

--- src/pages/SingleBook.jsx
+++ src/pages/SingleBook.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import {useParams} from 'react-router-dom'
-import BookContext from '../context/BookContext'
 import axios from 'axios'
 
 function SingleBook() {
